6aabe29d62e44cffd27102dde89a1ff1589351ba
zuyinamirkham <irhamzuyina@gmail.com>
2026-08-29 10:30:02 +0700

    Add files via upload

---
+++ proyek 1/src/main/java/com/mycompany/irkham_project1/Irkham_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.irkham_project1;
+
+/**
+ *
+ * @author acer
+ */
+public class Irkham_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Zuyina M. Irkham");
+        System.out.println("NPM : 2505060041");
+        System.out.println("Alamat : Kaliabu");
+        System.out.println("No_HP : 0834532232");
+    }
+}
